64dc1e6fff416fba9dfbdc861b1e9d3f419f45a7
Charan <charankulal0241@gmail.com>
2024-01-31 19:00:56 +0530

    Coded auth provider class for signing in and authentication purposes.

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: constant_identifier_names, no_leading_underscores_for_local_identifiers, avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+enum AuthStatus {
+  NotAuthenticated,
+  Authenticating,
+  Authenticated,
+  UserNotFound,
+  Error,
+}
+
+class AuthProvider extends ChangeNotifier {
+  late User user;
+  late AuthStatus status;
+  late FirebaseAuth _auth;
+  static AuthProvider instance = AuthProvider();
+
+  AuthProvider() {
+    _auth = FirebaseAuth.instance;
+  }
+
+  void loginUserWithEmailAndPassword(String _email, String _password) async {
+    status = AuthStatus.Authenticating;
+    notifyListeners();
+    try {
+      UserCredential _result = await _auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
+      user = _result.user!;
+      status = AuthStatus.Authenticated;
+      print("Login successful");
+      // Navigate to Home page
+    } catch (e) {
+      print("Sign in failed");
+    }
+  }
+}
